43f4fe852532e2e304acc5cb4ee728cb16d061d1
aadebuger <zhvxxh@gmail.com>
2021-08-25 22:57:45 +0800

    first commit

---
+++ dash.sh
@@ -0,0 +1,12 @@
+sudo fdisk -l |grep sd 
+sudo fdisk -l |grep sd |wc
+sudo fdisk -l |grep "65 T" |wc
+sudo fdisk -l |grep "56 T" |wc
+df -lh |grep hdd
+df -lh |grep hdd |wc 
+sudo fdisk -l |grep "65 T" |sort
+sudo fdisk -l |grep "65 T" |wc
+df -lh |grep "3.7T" |wc 
+df -lh |grep "3.7T" | sort
+tail -n 2 $PLOT/nohup.out
+tail -n 2 $POOL/nohup.out
---
+++ plotenv.sh
@@ -0,0 +1,4 @@
+export PLOT=/home/aadebuger/newplot/chia-plotter/build
+export POOL=/home/aadebuger/linux
+export newdest=/mnt/hddk/ptm020818/
+export FORMATCMD=" sudo mkfs.ext4 -m 0 -T largefile4 -L  m3 "
